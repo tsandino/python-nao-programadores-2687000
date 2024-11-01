5053233f5f7cb81fc0ab98ee2fe084e9a8e38053
tsandino <71132875+tsandino@users.noreply.github.com>
2024-11-01 18:55:37 +0000

    numerico.py

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,15 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
+print(type(data_nascimento))
+print(type(idade_numerica))
+print(type(altura))
 
 # Realize uma operação entre dados do tipo string e inteiro
-
+nome = "Tiago"
+print(nome * 3)
 
 # Realize uma operação entre dados do tipo inteiro e float
+salario = 1000
+bonus = 54.50
+print(salario + bonus)
