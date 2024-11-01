e15e3ba6667159c46728c0a77bdbfcb414827b90
tsandino <71132875+tsandino@users.noreply.github.com>
2024-11-01 18:39:57 +0000

    variaveis.py

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -5,7 +5,15 @@
 
 # Declare variáveis do tipo string
 
+nome = "Tiago"
+objetivo = "Aprender Programação"
+print(nome,",", objetivo)
 # Declare variáveis de cada tipo numérico
-
+tempoano_interger = 2
+objetivo_salarioano = 100000.00
+print(nome,",",tempoano_interger,",",objetivo_salarioano)
 
 # Declare uma variável do tipo booleano
+objetivo_realizavel = True
+objetivo_fracasso = False
+print(nome,",", objetivo_realizavel)
