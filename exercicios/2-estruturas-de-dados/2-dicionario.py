30444b79593919309cd3cedc70a7ec00ad6ae207
tsandino <71132875+tsandino@users.noreply.github.com>
2024-11-07 16:53:06 +0000

    dicionario.py

--- exercicios/2-estruturas-de-dados/2-dicionario.py
+++ exercicios/2-estruturas-de-dados/2-dicionario.py
@@ -7,12 +7,14 @@ pessoa = {'nome':'Crislaine',
           'animal_estimacao':False}
 
 # Imprima na tela o valor equivalente a chave "hobby"
-
+print(pessoa['hobby'])
 
 # Imprima na tela uma lista apenas com os valores do dicionário
-
-
+pessoa_values = list(pessoa.values())
+print(pessoa_values)
 # Imprima na tela uma lista apenas com as chaves do dicionário
-
-
+pessoa_chaves =list(pessoa.keys())
+print(pessoa_chaves)
 # Insira um novo par chave-valor no dicionário
+pessoa ['ano_nascimento'] = 1996
+print(pessoa)
--- exercicios/2-estruturas-de-dados/projeto-tempo-para-estudos.py
+++ exercicios/2-estruturas-de-dados/projeto-tempo-para-estudos.py
@@ -1,6 +1,13 @@
 # Criaremos um script que imprimirá na tela o total de horas que uma pessoa estudou durante um determinado período:
 # 1. Crie uma variável chamada "nome" e, usando o método input(), atribua a ela um nome;
+nome = input('Qual seu nome? ')
 # 2. Crie uma variável chamada "total_dias" e, usando o método input(), solicite o total de dias dedicados ao estudo por semana;
+total_dias = input('Quantos dias por semana você costuma estudar? ')
 # 3. Crie uma variável chamada "total_horas" e, usanod o método input(), solicite a média de horas estudada por dia;
+total_horas = input('Em média, quantas horas você estuda por dia? ')
 # 4. Crie uma variável chamada "curso" e, usando o método input(), solicite o título do curso desejado;
+curso = input('O que você estuda atualmente? ')
 # 5. Imprima na tela uma frase informando o nome da estudante, o total_dias dedicados aos estudos, o total horas semanais e o curso.
+total_dias = int(total_dias)
+total_horas = int(total_horas)
+print(f'Olá, {nome}! Você costuma estudar {total_dias} dias por semana, cerca de {total_horas} horas diárias. Em média, você costuma estudar {total_dias*total_horas} horas por semana para o curso {curso}.')
