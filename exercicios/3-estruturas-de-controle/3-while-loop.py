ab8e791543626d923d13dbb2b898cd09521b9117
tsandino <71132875+tsandino@users.noreply.github.com>
2024-11-08 19:14:26 +0000

    while-loop.py

--- exercicios/3-estruturas-de-controle/3-while-loop.py
+++ exercicios/3-estruturas-de-controle/3-while-loop.py
@@ -1,7 +1,11 @@
 # Crie duas variáveis do tipo numérica, uma sinalizando a fase atual do curso e outra o nível final
-
+nivel_atual = 1
+nivel_final = 4
 
 # Crie um while loop que imprima na tela o nível atual
-
-
+while nivel_atual <= nivel_final:
+  print(f"Você subiu um nível. Seu nível atual é {nivel_atual}.")
+  nivel_atual += 1
+else:
+  print("Parabéns! Você concluiu o curso.")
 # Insira "else" no while loop anterior.
