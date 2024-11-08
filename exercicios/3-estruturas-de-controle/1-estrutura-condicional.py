2d50164e0e7cc18042382883bddef6f091e14168
tsandino <71132875+tsandino@users.noreply.github.com>
2024-11-08 17:36:20 +0000

    estrutura-condicional.py

--- exercicios/3-estruturas-de-controle/1-estrutura-condicional.py
+++ exercicios/3-estruturas-de-controle/1-estrutura-condicional.py
@@ -1,8 +1,14 @@
 # Declare 4 variáveis do tipo numérica
-
+x = 4
+y = 30
+a = 3
+e = 66
 
 # Crie uma estrutura condicional para comparar dois números
-
+if x > y:
+  print(f'A condição foi cumprida. O número {x} é maior que o número {y}.')
+else:
+  print(f'A condição não foi cumprida. O número {y} é maior que o número {x}.')
 
 # Se a condição for verdadeira, imprima na tela uma mensagem informando que a condição foi cumprida e informando o número de maior valor
 
